c4acd447c1f339d59eef3cd7cf786682a0e17b32
Josh K <jmkimball@gatech.edu>
2014-03-30 12:59:33 -0400

    Adding cards example from work session

---
+++ ToDoGlassExample/src/com/example/todoglass/SecondActivity.java
@@ -0,0 +1,14 @@
+package com.example.todoglass;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SecondActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.second_layout);
+	}
+
+}
